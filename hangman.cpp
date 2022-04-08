dcd677af67612fe03c445c6beaa796e9eeeff0d4
DoDucHuy21020124 <21020124@vnu.edu.vn>
2022-04-08 15:34:14 +0700

    DoDucHuy_21020124

--- hangman.cpp
+++ hangman.cpp
@@ -69,7 +69,9 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
     answer = wordList[index];
-    for ( int i = 0; i < answer.length(); ++i ) answer[i] = tolower(answer[i]);
+    for ( int i = 0; i < answer.length(); ++i ) {
+        if ( answer[i] >= 'A' && answer[i] <= 'Z' ) answer[i] = tolower(answer[i]);
+    }
     return answer;
 }
 
