74182ccb1b45e5aeca68c3ace6e604a8524f49b2
unknown <21020535@vnu.edu.vn>
2022-04-08 11:31:28 +0700

    small

--- hangman.cpp
+++ hangman.cpp
@@ -71,6 +71,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
+    answer = wordList[index];
 
     return answer;
 }
