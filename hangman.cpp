681f1c116120aae150b66920a05cad887365fa3d
TranManhDung21020119 <21020119@vnu.edu.vn>
2022-04-08 15:34:09 +0700

    21020119 Tran Manh Dung

--- hangman.cpp
+++ hangman.cpp
@@ -55,7 +55,7 @@ bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
     for (int i = 0; i < word.length(); i++){
-        if (ch == word.[i]){
+        if (ch == word[i]){
             return 1;
         }
     }
@@ -75,7 +75,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     string answer;
     answer = wordList.at(index);
     for (int i = 0; i < answer.length(); i++){
-        if (answer[i] >= "A" && answer[i] <= "Z"){
+        if (answer[i] >= 'a' && answer[i] <= 'z'){
             answer[i] = answer[i] + 32;
         }
     }
