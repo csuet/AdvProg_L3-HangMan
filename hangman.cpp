71281f3462670db28d9ce2fb52760790b9d82e14
huy165273 <21020765@vnu.edu.vn>
2022-04-09 23:32:31 +0700

    hang man

--- hangman.cpp
+++ hangman.cpp
@@ -19,6 +19,7 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
+    srand((int)time(0));
     int number= min + rand()%(max-min+1);
     return number;
 }
