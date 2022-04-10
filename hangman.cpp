5cac65370cdeefaefb956fe22685b39b3a19750c
Samoyed23 <91705341+Samoyed23@users.noreply.github.com>
2022-04-10 10:19:31 +0700

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -160,7 +160,7 @@ void processData(const char ch, const string& word,
         updateSecretWord(secretWord, ch, word);
         updateEnteredChars(ch, correctChars);
     } else {
-        updateIncorrectGuess();
+        updateIncorrectGuess(incorrectGuess);
         updateEnteredChars(ch, incorrectChars);
     }    
 }
