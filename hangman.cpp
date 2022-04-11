b357b0ba4b83b62b83daa7e7c99275eac34246bc
huy165273 <21020765@vnu.edu.vn>
2022-04-11 22:42:07 +0700

    hang man

--- hangman.cpp
+++ hangman.cpp
@@ -163,7 +163,7 @@ void processData(const char ch, const string& word,
        updateEnteredChars(ch, correctChars);
    }
    else{
-       updateIncorrectGuess(incorrectChars);
+       updateIncorrectGuess(incorrectGuess);
        updateEnteredChars(ch, incorrectChars);
    }
 }
