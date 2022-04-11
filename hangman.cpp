3a24acc72d7d875a13eab7165c59a6f3ab032c51
quyenhoang03 <21020787@vnu.edu.vn>
2022-04-11 12:33:40 +0700

    Test

--- hangman.cpp
+++ hangman.cpp
@@ -170,7 +170,7 @@ void processData(const char ch, const string &word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
-    if (isCharInWord(ch, word) == true)
+    if (isCharInWord(ch, word))
     {
         updateSecretWord(secretWord, ch, word);
         updateEnteredChars(ch, correctChars);
