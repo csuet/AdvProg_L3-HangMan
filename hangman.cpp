9feb2fb1c933f86d2848ef78c425c3e9ad6df91c
quyenhoang03 <21020787@vnu.edu.vn>
2022-04-11 12:06:45 +0700

    Hangman

--- hangman.cpp
+++ hangman.cpp
@@ -69,7 +69,7 @@ string chooseWordFromList(const vector<string> &wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
 
-    string answer = wordList.[index];
+    string answer = wordList[index];
     for (int i = 0; i < answer.length(); i++)
     {
         if (answer[i] >= 'A' && answer[i] <= 'Z')
