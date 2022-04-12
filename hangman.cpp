7fd251299441d33fd800bb8bbcc7b44970f3d93c
tuananhtran21020280 <100192298+tuananhtran21020280@users.noreply.github.com>
2022-04-12 09:21:49 +0700

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -52,7 +52,7 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
-       for(int i = 0; i < word.length(); I++)
+       for(int i = 0; i < word.length(); i++)
     {
         if(ch == word[i]
 		
