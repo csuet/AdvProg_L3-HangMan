10c74fb2468a54955450328ff20d44c4d8fce506
MaiThanhHai <maithanhhai2103@gmail.com>
2022-04-20 19:25:04 +0700

    @@

--- hangman.cpp
+++ hangman.cpp
@@ -83,7 +83,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 ***/
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
-    string secretWord;
+    string secretWord = answerWord;
     for (int i = 0; i < answerWord.size(); i++) {
         secretWord[i] = '-';
     }
@@ -112,8 +112,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
             int pos;
             if (word[i] == ch) pos = i;
             secretWord[pos] = ch;
-        }
-        
+        } 
     } 
 }
 
