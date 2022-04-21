6788a692b76583e8c82a82650031988a5010e62e
lnhi <nhiinguyen2406@gmail.com>
2022-04-21 10:06:51 +0700

    Nguyen Thi Lan Nhi-21020372

--- hangman.cpp
+++ hangman.cpp
@@ -83,8 +83,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-    for(int i=0;i<answerWord.length();i++)
-      secretWord[i]='-';
+    secretWord=string(answerWord.length(),'-');
     return secretWord;
 }
 
