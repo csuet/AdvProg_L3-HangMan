5a55c641a2275e1b2d9b3114710ff76253e17b0b
ntthong <21020408@vnu.edu.vn>
2022-04-23 21:14:55 +0700

    fixed

--- hangman.cpp
+++ hangman.cpp
@@ -98,7 +98,7 @@ string chooseWordFromList(const vector<string> &wordList, int index)
 string generateHiddenCharacters(string answerWord)
 {
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
-    string secretWord = string(answerWord.length(), '-')
+    string secretWord = string(answerWord.length(), '-');
 
         return secretWord;
 }
