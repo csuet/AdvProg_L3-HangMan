af6f248cf81c9aed9923d7daa9b6c229d1be5910
Thanh Tung <21020399@vnu,edu.vn>
2022-04-26 00:20:04 +0700

    donee

--- hangman.cpp
+++ hangman.cpp
@@ -164,7 +164,7 @@ void processData(const char ch, const string& word,
         updateEnteredChars(ch,correctChars);
     }
     else {
-        updateIncorrectGuess(incorrectChars);
+        updateIncorrectGuess(incorrectGuess);
         updateEnteredChars(ch,incorrectChars);
     }
     /*** TODO
